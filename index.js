6e0368cac03689e0435b20f81b2e915a1745dfd5
Arunkumar C <you@example.com>
2016-12-09 21:23:29 +0530

    bot

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/webhook', function (req, res) {
     }
 });
 
-app.get('/webhook', function (req, res) {
+app.get('/requestdata', function (req, res) {
     request('http://www.google.com', function (error, response, body) {
   if (!error && response.statusCode == 200) {
     console.log(body) // Show the HTML for the Google homepage.

73c0828b0bd390afd829b3356380d9241405ca80
Arunkumar C <you@example.com>
2016-12-09 21:19:56 +0530

    bot

--- index.js
+++ index.js
@@ -21,6 +21,14 @@ app.get('/webhook', function (req, res) {
     }
 });
 
+app.get('/webhook', function (req, res) {
+    request('http://www.google.com', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body) // Show the HTML for the Google homepage.
+  }
+})
+});
+
 app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
     var botmsg ="variable value";

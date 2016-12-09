c85f20512c2a7c4952dd4af2335a55b170e19149
Arunkumar C <you@example.com>
2016-12-10 01:19:16 +0530

    bot

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get('/mydat', function(req, res) {
  }
   }).pipe(res);
 
-      var bodycntnt = req.body.entry;
+      var bodycntnt = req.body;
       res.send(bodycntnt);
  var btmgar="passing msg";
 });

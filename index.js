ef7f79ee38dfa53ec538650b78e3915d7fd261b8
Arunkumar C <you@example.com>
2016-12-23 20:07:08 +0530

    Create Echo Bot

--- index.js
+++ index.js
@@ -26,13 +26,10 @@ app.get('/webhook', function (req, res) {
 
 app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
-    var botmsg ="variable value";
-
     for (i = 0; i < events.length; i++) {
         var event = events[i];
-
         if (event.message && event.message.text) {
-            sendMessage(event.sender.id, {text: "Echo: " + message});
+            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
         }
     }
     res.sendStatus(200);
@@ -55,7 +52,6 @@ function sendMessage(recipientId, message) {
         }
     });
 };
-
 
 
 

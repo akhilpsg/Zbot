88e4401d1a6e0dd5c715948aeb23b1840d1e176a
Arunkumar C <you@example.com>
2017-01-06 17:26:34 +0530

    bot

--- index.js
+++ index.js
@@ -88,10 +88,10 @@ function sendMessage(recipientId, message) {
 
 function getCreator() {
     request({
-        url: 'http://api.icndb.com/jokes/random',
+        url: 'https://creator.zoho.com/api/json/vendor/view/Item_View?scope=creatorapi&authtoken=dba9eaaf1528a1c77885e321fa85e44e&zc_ownername=akhilp2&raw=true',
     }, function(error, response, body){
        re = JSON.parse(body);
-        resjoke =re.value.joke;
+        resjoke =re;
     });
     return resjoke;
 };

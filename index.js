38fa485ecf173b8c1a2ac17666e35bd2211454f9
Arunkumar C <you@example.com>
2017-01-10 21:35:35 +0530

    bot

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/webhook', function (req, res) {
     } else {
         res.send('Invalid verify token');
     }
-tmpmsg ='{"attachment": {"type": "template","payload": {"elements": [{"title": "Kitten", "subtitle": "Cute kitten picture","buttons":[{"type":"web_url","url": imageUrl, "title": "Show kitten"}, {"type": "postback","title": "I like this","payload": "User " + recipientId + " likes kitten" + imageUrl,}]}] }}};'
+tmpmsg ='{"attachment": {"type": "template","payload": {"elements": [{"title": "Kitten", "subtitle": "Cute kitten picture","buttons":[{"type":"web_url","url": imageUrl, "title": "Show kitten"}, {"type": "postback","title": "I like this","payload": "User "}]}] }}};'
             sendMessage("1136970429751020", tmpmsg);
 
 

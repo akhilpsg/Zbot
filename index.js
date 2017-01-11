a5906368ea651d6c261baafafde177206878d4b9
Arunkumar C <you@example.com>
2017-01-11 17:09:54 +0530

    testbot

--- index.js
+++ index.js
@@ -105,8 +105,8 @@ var fbuserid = event.sender.id;
           }
           else if(event.message.text.toUpperCase()=='HI' || event.message.text.toUpperCase()=='HELLO'){ 
                var noresults =event.message.text+ " "+fbuserfname "!, "+"I am Zbot and I am designed to serve your buiness needs. Please choose the appropriate option to boot your business operating system";
-          
-                            sendMessage(event.sender.id, {text: noresults});
+           var botqus = '{"attachment": { "type": "template", "payload": { "template_type": "button", "text": "'+noresults+'", "buttons": [{ "type": "postback", "title": "Product Name", "payload": "Enter Your Product Name" }, { "type": "postback", "title": "Product Code", "payload": "Enter Your Product Code" }] } } }';
+                            sendMessage(event.sender.id, botqus);
 
           }
           else{
